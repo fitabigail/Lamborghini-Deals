b3a6255b37a9a3eab49f5d295f989c8e9ef7d263
fitabigail <abidinu35@gmail.com>
2023-01-23 11:00:13 +0000

    Add requirements.txt  file

--- lamborghini/settings.py
+++ lamborghini/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -32,8 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['lamborghini-deals.herokuapp.com', 'localhost']
-
-
 
 
 # Application definition
@@ -67,7 +63,7 @@ ROOT_URLCONF = 'lamborghini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR ],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
